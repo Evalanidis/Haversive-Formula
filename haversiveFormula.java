4863126adbb209d2f8545e3395409a2ff23fa026
Evalanidis <evalanidis@gmail.com>
2017-10-24 21:18:12 +0300

    Update haversiveFormula.java

--- haversiveFormula.java
+++ haversiveFormula.java
@@ -1,16 +1,25 @@
+/*
+* @author Valanidis Efstathios
+* @literal evalanidis@gmail.com
+* @since 24/10/2017
+* */
+
 static double earthRadius = 6371.0;
-static double latitude1,latitude2,longitude1,longitude2,dLat,dLong,hav1,hav2,sum,d;
+static double lat1,lat2,latitude1,latitude2,longitude1,longitude2,dLat,dLong,hav1,hav2,sum,d;
 
 public static void haversiveFormula(){
-        System.out.print("What is the latitude of your location in degrees? ");
+        System.out.print("What is the latitude of the first location in degrees? ");
         latitude1 = input.nextDouble();
-        System.out.print("What is the longitude of your location in degrees? ");
+        System.out.print("What is the longitude of the first location in degrees? ");
         longitude1 = input.nextDouble();
-        System.out.print("What is the latitude of your destination in degrees? ");
+        System.out.print("What is the latitude of the second location in degrees? ");
         latitude2 = input.nextDouble();
-        System.out.print("What is the longitude of your destination in degrees? ");
+        System.out.print("What is the longitude of the second location in degrees? ");
         longitude2 = input.nextDouble();
-
+        
+        lat1=latitude1;
+        lat2=latitude2;
+        
         dLat = Math.toRadians(latitude2 - latitude1);
         dLong = Math.toRadians(longitude2 - longitude1);
         latitude1 = Math.toRadians(latitude1);
@@ -23,8 +32,8 @@ public static void haversiveFormula(){
         d = 2 * Math.atan2(Math.sqrt(sum),Math.sqrt(1-sum));
         d = (float)(d * earthRadius);
 
-        System.out.print("Your location's latitude is "+latitude1+" and longitude is "+longitude1+"\n");
-        System.out.print("Your destination's latitude is "+latitude2+" and longitude is "+longitude2+"\n");
-        System.out.printf("Using the Haversive formula the distance between your location and your destination is %.2f km",d);
+        System.out.print("The first location's latitude is "+latitude1+" and longitude is "+longitude1+"\n");
+        System.out.print("The second location's latitude is "+latitude2+" and longitude is "+longitude2+"\n");
+        System.out.printf("Using the Haversive formula the distance between the two locations is %.2f km",d);
 
     }
